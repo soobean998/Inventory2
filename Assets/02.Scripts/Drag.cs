d53c89453dd89192c7a1600e8988841afe6de31e
Soobean Park <psb3333@naver.com>
2021-04-26 18:51:05 +0900

    0426

--- Assets/02.Scripts/Drag.cs
+++ Assets/02.Scripts/Drag.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
+    public static GameObject draggingItem = null;
+    private Transform itemLisTr;
+    private Transform inventoryTr;
+
     void Start()
     {
-        
+        inventoryTr = GameObject.Find("Inventory").transform;
+        itemLisTr = GameObject.Find("ItemList").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+       draggingItem = this.gameObject;
+       GetComponent<CanvasGroup>().blocksRaycasts = false;
+       transform.SetParent(inventoryTr);
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       this.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        draggingItem = null;
+       GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+       if(transform.parent ==inventoryTr)
+       {
+           transform.SetParent(itemLisTr);
+       }
+    }
+
+  
 }
